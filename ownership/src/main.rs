bdf9d0925809108c79882031b84e2bc2580571e0
Maxime <maximechagnon00@gmail.com>
2024-11-06 14:21:14 +0100

    TP1

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,58 @@
+#[derive(Clone)]
+#[derive(Copy)]
+struct Rectangle {
+    length: f64,
+    width: f64,
+}
+
+fn perimeter(rect : Rectangle) -> f64 {
+    2.0 * (rect.length + rect.width)
+}
+
+fn perimeter2(rect : &Rectangle) -> f64 {
+    2.0 * (rect.length + rect.width)
+}
+
+fn average(a : f64, b : f64) -> f64 {
+    (a+b) / 2.0
+}
+
+fn print_references() {
+    let x = 18;
+    // let ref1 = &x;
+    // let ref2 = &x;
+    let mut ref3 = &x;
+    let mut ref4 = &x;
+    println!("{} {}", ref3, ref4)
+}
+
+fn swap(x: &mut f64, y: &mut f64) {
+    let temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = 4.2;
+    let y = 5.8;
+
+    println!("{}", average(x, y));
+
+    let rect = Rectangle{length: 2.0, width: 6.0};
+    
+    println!("{}", perimeter(rect.clone()));
+
+    let rect2 = Rectangle{length: 2.0, width: 6.0};
+    
+    println!("{}", perimeter2(&rect2.clone()));
+
+    println!("---------------------------------------------------");
+
+    print_references();
+
+    let mut x = 4.0;
+    let mut y = 5.0;
+    swap(&mut x, &mut y);
+
+    println!("x: {}, y: {}", x, y)
 }
