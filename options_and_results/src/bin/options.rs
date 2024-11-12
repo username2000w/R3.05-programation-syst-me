c938aefb02f49d66ac1c5e42304640f341a9cc77
Maxime <maximechagnon00@gmail.com>
2024-11-12 14:20:44 +0100

    feat: TP2.2.3

--- options_and_results/src/bin/options.rs
+++ options_and_results/src/bin/options.rs
@@ -7,6 +7,9 @@ fn main() {
 
     println!("{}", String::from(print_first_word2(sentence1)));
     // println!("{}", String::from(print_first_word2(sentence2)));
+
+    iterate_over_words(sentence1);
+    iterate_over_words(sentence2);
 }
 
 fn print_first_word1(sentence: &str) {
@@ -18,4 +21,10 @@ fn print_first_word1(sentence: &str) {
 
 fn print_first_word2(sentence: &str) -> &str {
     sentence.split_whitespace().next().expect("La chaîne dois être non vide")
+}
+
+fn iterate_over_words(sentence: &str) {
+    for word in sentence.split_whitespace() {
+        println!("Mot: {}", word);
+    }
 }
